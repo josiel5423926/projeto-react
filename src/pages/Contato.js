44dee4644f73f95ced7408bfce38b18792f90cd5
Josiel Souza <josielsouza.dj@gmail.com>
2021-02-12 21:46:48 -0300

    atualizar pagina inicial

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,5 +1,7 @@
 import { Container, Col} from 'react-bootstrap';
-import Logo from './logo.png';
+//import Logo from './logo.png';
+import Email from './email.png';
+import FormaPagamento from './formas-de-pagamento.png';
 
 
 export default function Contato() {
@@ -10,13 +12,25 @@ export default function Contato() {
                 <h1 className="py-4">Fale Conosco</h1>
             </div>
             
-             <Col className="mx-auto my-5" sm={12} md={6} lg={6}>
-                            <a href="/">
-                                <img src={Logo} width="150px" alt="&copy; Full Stack Motos" title="&copy; Full Stack Motos" className="shadow-sm bg-white rounded" />
-                            </a>
-                            <a href="/">
-                                <img src="./logo.png" width="150px" alt="&copy; Full Stack Motos" title="&copy; Full Stack Motos" className="shadow-sm bg-white rounded" />
-                            </a>
+             <Col className="mx-auto my-5 row" sm={12} md={6} lg={6}>
+
+
+             <div className="py-4 col-sm-6">
+                <h4 className="py-4">Fullstackmotos@gmail.com</h4>
+                <a href="/">
+                    <img src={Email} width="150px" alt="&copy; Full Stack Motos" title="&copy; Full Stack Motos" className="shadow-sm bg-white rounded" />
+                </a>
+            </div>
+
+            <div className="py-4 col-sm-6">
+                <h4 className="py-4">(11) 9 5088-6634</h4>
+
+                <a href="/">
+                    <img src={FormaPagamento} width="200px" alt="&copy; Full Stack Motos" title="&copy; Full Stack Motos" className="shadow-sm bg-white rounded" />
+                </a>
+            </div>
+            
+                           
              </Col>
 
             
